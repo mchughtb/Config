23d58ee57ec7616b8d216c45685485e808bc8e56
Robert Byrne <robert@byrnemail.org>
2014-05-30 01:01:29 +0100

    Add periodic files for fliss

---
+++ Periodic/fliss.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+
+
+set -u
+export PATH=$HOME/scripts/Darwin:$HOME/scripts:$HOME/bin:$PATH
+#date "+%Y-%m-%d %H.%M.%S Skipping skipping skipping
+#exit"
+date "+%Y-%m-%d %H.%M.%S Starting Periodic"
+
+flags="$@"
+
+jobRunner2.sh -n "tv-rssd" -t 23 $flags $HOME/scripts/tv-rss.sh -D
+jobRunner2.sh -n "tv-rss" -t 13 $flags $HOME/scripts/tv-rss.sh
+jobRunner2.sh -n "picasa-ucecrjf" -t 1442  $flags $HOME/scripts/photo-fetch.sh -d "$HOME/Pictures/ServerFiles/Originals/%Y/%Y-%m-01-KelliPicasa" -u ucecrjf -s picasa -L KelliPicasa
+jobRunner2.sh -n "flickr-les" -t 1444  $flags $HOME/scripts/photo-fetch.sh -s flickr -d "$HOME/Pictures/ServerFiles/Originals/%Y/%Y-%m-01-LesFlickr" -u "45232884@N02" -L LesFlickr
+jobRunner2.sh -n "flickr-kelli" -t 1445  $flags $HOME/scripts/photo-fetch.sh -s flickr -d "$HOME/Pictures/ServerFiles/Originals/%Y/%Y-%m-01-KelliFlickr" -u "41352893@N00" -L KelliFlickr
+jobRunner2.sh -n "flickr-dan" -t 1447  $flags $HOME/scripts/photo-fetch.sh -s flickr -d "$HOME/Pictures/ServerFiles/Originals/%Y/%Y-%m-01-DanFlickr" -u "49392099@N02" -L DanFlickr
+jobRunner2.sh -n "small-photos" -t 61 $flags $HOME/scripts/photo-thumbnails.sh "$HOME/Pictures/Photos/Originals" Originals SmallPhotos
+jobRunner2.sh -n "sync-appletv" -t 15  $flags $HOME/scripts/sync-appletv.sh
+jobRunner2.sh -m debs -n "backup-fliss" -t 121  $flags $HOME/scripts/backup-fliss.sh
+#jobRunner2.sh -n "move-watched-shows" -t 1449 $flags $HOME/scripts/moveUnlinkedFiles.sh
+date "+%Y-%m-%d %H.%M.%S Ending Periodic"
