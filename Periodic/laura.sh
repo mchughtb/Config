b9712ef912c047865eef4c89ab2a6e5424d57672
Robert Byrne <robert@byrnemail.org>
2014-08-03 18:57:57 +0100

    misc stuff

--- Periodic/laura.sh
+++ Periodic/laura.sh
@@ -9,11 +9,11 @@ syncHost=fliss
 flags="$@"
 date "+%Y-%m-%d %H.%M.%S Starting Periodic"
 
-jobRunner2.sh -m $syncHost -n "sync-documents" -t 12 $flags $HOME/scripts/sync-documents.sh
+jobRunner2.sh -m $syncHost -n "sync-documents" -t 13 $flags $HOME/scripts/sync-documents.sh
 jobRunner2.sh -m $syncHost -n "sync-photos" -t 73 $flags $HOME/scripts/sync-photos.sh
-jobRunner2.sh -m $syncHost -n "sync-queue" -t 25 $flags $HOME/scripts/sync-queue.sh
-jobRunner2.sh -m $syncHost -n "sync-music" -t 65 $flags $HOME/scripts/sync-music.sh
-jobRunner2.sh -m $syncHost -n "sync-imovie" -t 1340 $flags $HOME/scripts/sync-imovie.sh
+jobRunner2.sh -m $syncHost -n "sync-queue" -t 23 $flags $HOME/scripts/sync-queue.sh
+jobRunner2.sh -m $syncHost -n "sync-music" -t 61 $flags $HOME/scripts/sync-music.sh
+jobRunner2.sh -m $syncHost -n "sync-imovie" -t 1341 $flags $HOME/scripts/sync-imovie.sh
 
 date "+%Y-%m-%d %H.%M.%S Ending Periodic"
 
