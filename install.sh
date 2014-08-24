9230e0b61d67a72b68b24f3e2a90ba526ee3b1b3
Robert Byrne <robert@byrnemail.org>
2014-08-24 17:31:41 +0100

    Use colon in place of | and support regexps in host/uname patterns
    * replace pipes in config file with colons
    * tidy up the help text
    * Improve error messages
    * get the hostname compatible with msys bash

--- install.sh
+++ install.sh
@@ -2,8 +2,6 @@
 set -u -e
 
 # reads a file of links to set up relative to a source and dest folder
-# file is a pipe seperated list of destination|source
-#	 .bashrc|my_bashrc.bash
 # the filenames are relative to the source and dest folders  (to make it possible 
 # to test it without trashing the real files). If unspecified the dest is $HOME
 # and the source is the same folder that the script is in
@@ -16,24 +14,35 @@ srcdir="$thisdir"
 destdir="$HOME"
 config="install.cfg"
 dryRun=
-copyMode=
+copyMode="link"
 readonly backupname=$(date +"backups/%Y%m%d-%H%M%S")
 verbose=
-host=$( hostname -s )
+host=$( hostname -s 2> /dev/null || hostname )
 os=$( uname )
 
 function usage()
 {
 	cat <<-END
-	Usage:
-	$0 -nv -s source_dir -d dest_dir -m hostname -o os configfile
-	     -n  dry run
-	     -v  verbose
-		 -c copy files instead of linking
-	     -m  override machine hostname (defaults to $host)
-	     -o  override operating system (defaults to $os)
-	     source_dir defaults to $thisdir
-	     dest_dir defaults to $HOME
+
+	Usage:     $0 -nv -s source_dir -d dest_dir -m hostname -o os configfile
+
+	           -n     dry run, don't make any changes
+	           -s     source dirrectory. Defaults to $thisdir
+	           -d     destination directory. Defaults to $HOME
+	           -c     copy files instead of linking
+	           -v     verbose output
+	           -m     override machine hostname (defaults to $host)
+	           -o     override operating system (defaults to $os)
+	           -h     display this message
+
+	       configfile is a colon seperated list of destination:source:host_pattern:os_pattern
+
+	         .bashrc:my_bashrc.bash:satur.*:Linux|Darwin
+
+	       will link/copy source_dir/my_bashrc.bash to dest_dir/.bashrc if the hostname 
+	       begins with satur (saturday, saturn etc..) and uname is either Darwin or Linux. 
+	       Patterns are bash ERE but a single * will match any host/platform
+
 	END
 }
 
@@ -44,14 +53,14 @@ function process_args()
 		s) srcdir=${OPTARG%/} ;;
 		d) destdir=${OPTARG%/} ;;
 		n) dryRun=1 ;;
-		c) copyMode=1 ;;
+		c) copyMode='copy' ;;
 		v) verbose=1 ;;
 		o) os=${OPTARG} ;;
 		m) host=${OPTARG} ;;
 		h) usage ; exit 1 ;;
 		:) echo "ERROR: missing argument to flag [-$OPTARG]" ; usage ; exit 5 ;;
-	\?) echo "ERROR: Invalid Option [-$OPTARG]" ; usage ; exit 5 ;;
-esac
+		\?) echo "ERROR: Invalid Option [-$OPTARG]" ; usage ; exit 5 ;;
+		esac
 	done
 
 	shift $((OPTIND-1))
@@ -61,9 +70,9 @@ esac
 
 function linker()
 {
-	if [[ $copyMode ]] ; then
+	if [[ $copyMode == copy ]] ; then
 		echo COPY: "$@"
-		[[ $dryRun ]] || cp -Rn "$@"
+		[[ $dryRun ]] || cp -R "$@"
 	else
 		echo LINK: "$@"
 		[[ $dryRun ]] || ln -s "$@"
@@ -83,36 +92,45 @@ function log()
 	fi
 }
 
+function createdir()
+{
+	# dont print anything during dryrun or we get repeated messages about the same dir
+	if [[ ! -d "$@" ]] ; then
+		[[ $dryRun ]] || { log "CREATE: $@" ; mkdir -p "$@" ; }
+	fi
+}
+
 function main() {
 	log "destdir:     $destdir"
 	log "srcdir:      $srcdir"
 	log "config:      $config"
 	log "dryRun:      $dryRun"
+	log "link:        $copyMode"
+	log "os:          $os "
 	log "host:        $host"
-	log "os:          $os"
 
-	[[ -r "$config" ]] || { echo "ERROR: cannot read config: $config" ; exit 6 ; }
-	[[ -d "$srcdir" ]] || { echo "ERROR: source dir not a dir?: $srcdir" ; exit 7 ; }
-	[[ -d "$destdir" ]] || { echo "ERROR: dest dir not a dir?: $destdir" ; exit 7 ; }
+	[[ -r "$config" ]]  || { echo "ERROR: cannot read config: $config"    ; exit 6 ; }
+	[[ -d "$srcdir" ]]  || { echo "ERROR: source dir not a dir?: $srcdir" ; exit 7 ; }
+	[[ -d "$destdir" ]] || { echo "ERROR: dest dir not a dir?: $destdir"  ; exit 7 ; }
 
-	IFS='|'
+	IFS=':'
 	grep -v '^#' "$config" | while read dest src hostpattern ospattern; do
-		log "========================"
+		[[ $dryRun ]] || log "========================"
 		[[ ! -z "$src" && ! -z "$dest" ]] || continue 
-		[[ "$host" == $hostpattern ]] || { log "SKIP: $src for host $hostpattern" ; continue; }
-		[[ "$os" == $ospattern ]] || { log "SKIP: $src for os $ospattern" ; continue; }
+		[[ "$hostpattern" == "*" || "$host" =~ $hostpattern ]] || { log "SKIP: $src -> $dest no match for $host =~ $hostpattern"   ; continue; }
+		[[ "$ospattern" == "*"   || "$os" =~ $ospattern ]]     || { log "SKIP: $src -> $dest no match for $os =~ $ospattern"       ; continue; }
 		local srcfile="$srcdir/$src"
 		local destfile="$destdir/$dest"
-		[[ -r "$srcfile" ]] || { echo "ERROR: unreadable source:$srcfile" ; continue ; }
+		[[ -r "$srcfile" ]] || { echo "ERROR: $src -> $dest sourcefile $srcfile is not readable" ; continue ; }
 		if [[ -e "$destfile" || -L "$destfile" ]] ; then
 			if [[ "$srcfile" -ef "$destfile" ]] ; then
-				log "SKIP: Already link to sourcefile :$destfile"
+				log "SKIP: $src -> $dest dest $destfile is already linked to sourcefile: $srcfile"
 				continue
 			fi
 			remove "$destfile" 
 		else
 			local dir="${destfile%/*}"
-			[[ -d "$dir" ]] || { log "CREATE: $dir" ; mkdir -p $dir ; }
+			createdir "$dir"
 		fi
 		linker "$srcfile" "$destfile"
 	done
