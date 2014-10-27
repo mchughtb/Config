bce10d958c912727cd12e3645cd48ce025e8b82b
rb90318 <robert.byrne@citi.com>
2014-10-27 09:08:51 +0000

    Citi Specific Setup

--- Bash/local.citivm.bash
+++ Bash/local.citivm.bash
@@ -2,7 +2,7 @@
 export PS1='\[\e[1;32m\]\u@cmd-realtime-dev: \w/ >\[\e[0m\] '
 
 export http_proxy=http://webproxy.ssmb.com:8080/
-export https_proxy=http://webproxy.ssmb.com:8080/
+export https_proxy=https://webproxy.ssmb.com:8080/
 alias vi=vim
 export PATH="$PATH:/opt/mongo/bin"
 
