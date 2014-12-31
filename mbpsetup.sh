c7073279337f2aa41e9fc22788c8dbfaca4ca1d6
Robert Byrne <robert@byrnemail.org>
2014-12-31 15:23:10 +0000

    Setup improvements

--- mbpsetup.sh
+++ mbpsetup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -eu
 
+dryrun=1
+
 function once() {
     local title="$1"
     local check="$2"
@@ -10,9 +12,9 @@ function once() {
         echo "$title: already installed"
     else
         echo "================ INSTALL     $title ==============="
-        $cmd
+        echo  $cmd
         if [[ -z "$post" ]] ; then
-            $post
+            echo $post
         fi
     fi
     echo "================ END         $title ==============="
@@ -23,6 +25,7 @@ function inst_brew() {
     local pkg="$1"
     local cmd="brew install $1"
     local check="brew ls $1"
+    local post="${4:-""}"
     once "$title" "$check" "$cmd"
 }
 
@@ -35,15 +38,23 @@ function inst_cask() {
 }
 
 
+function inst_gem() {
+    local title="$1"
+    local pkg="$1"
+    local cmd="/usr/local/bin/gem install $1"
+    local check="/usr/local/bin/gem search -i $1"
+    once "$title" "$check" "$cmd"
+}
+
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 once "xcode" "xcode-select -p" "xcode-select --install"
 once "homebrew" "which -s brew" "ruby -e '$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)'" "brew update"
-inst_brew "caskroom/cask/brew-cask"
+inst_brew "caskroom/cask/brew-cask" "brew cask update"
 
 #
 # Must haves brew
 #
-for brew in "bash-completion" git ruby tmux "git-sh" ack the_silver_searcher ; do
+for brew in "bash-completion" git ruby tmux "git-sh" the_silver_searcher ; do
     inst_brew "$brew"
 done
 
@@ -54,20 +65,20 @@ for cask in macvim google-chrome alfred flux spectacle unison nvalt ; do
     inst_cask "$cask"
 done
 
-#
-#  Nice brews
-#
-for brew in npm nmap xmlstartlet imagemagick exiftool colordiff subversion trash multimarkdown ; do
-    inst_brew "$brew"
-done
+# #
+# #  Nice brews
+# #
+# for brew in node reattach-to-user-namespace ssh-copy-id terminal-notifier wget nmap xmlstartlet imagemagick exiftool colordiff subversion trash multimarkdown ; do
+#     inst_brew "$brew"
+# done
 
-#
-#  Nice casks
-#
-for cask in launchrocket vlc virtualbox transmission xamarin-studio adium ; do
-    inst_cask "$cask"
-done
+# #
+# #  Nice casks
+# #
+# for cask in launchrocket vlc virtualbox transmission xamarin-studio adium vagrant xquartz ; do
+#     inst_cask "$cask"
+# done
 
 for gem in bundler ; do
-    /usr/local/bin/gem install bundler
+    inst_gem bundler
 done
