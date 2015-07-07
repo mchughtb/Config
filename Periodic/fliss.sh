5334cf25f68be3edd2ad2f4aa1f941834b810009
Robert Byrne <robert@byrnemail.org>
2015-07-07 07:15:35 +0100

    Put all fetched photos in same folders. Not split by source

--- Periodic/fliss.sh
+++ Periodic/fliss.sh
@@ -12,11 +12,11 @@ flags="$@"
 
 # jobRunner2.sh -n "tv-rssd" -t 17 $flags $HOME/scripts/tv-rss.sh -D
 # jobRunner2.sh -n "tv-rss" -t 16 $flags $HOME/scripts/tv-rss.sh
-jobRunner2.sh -n "eztv-rss" -t 13 $flags $HOME/scripts/eztv-rss.sh -s $HOME/Movies/favourites.txt
-jobRunner2.sh -n "eztv-rss-yesterday" -t 360 $flags $HOME/scripts/eztv-rss.sh -s $HOME/Movies/favourites.txt -t 1
+#jobRunner2.sh -n "eztv-rss" -t 13 $flags $HOME/scripts/eztv-rss.sh -s $HOME/Movies/favourites.txt
+#jobRunner2.sh -n "eztv-rss-yesterday" -t 360 $flags $HOME/scripts/eztv-rss.sh -s $HOME/Movies/favourites.txt -t 1
 jobRunner2.sh -n "picasa-ucecrjf" -t 1442  $flags $HOME/scripts/photo-fetch.sh -d "$HOME/Pictures/ServerFiles/Originals/%Y/%Y-%m-01-KelliPicasa" -u ucecrjf -s picasa -L KelliPicasa
 jobRunner2.sh -n "flickr-les" -t 1444  $flags $HOME/scripts/photo-fetch.sh -s flickr -d "$HOME/Pictures/ServerFiles/Originals/%Y/%Y-%m-01-LesFlickr" -u "45232884@N02" -L LesFlickr
-jobRunner2.sh -n "flickr-kelli" -t 1445  $flags $HOME/scripts/photo-fetch.sh -s flickr -d "$HOME/Pictures/ServerFiles/Originals/%Y/%Y-%m-01-KelliFlickr" -u "41352893@N00" -L KelliFlickr
+jobRunner2.sh -n "flickr-kelli" -t 1445  $flags $HOME/scripts/photo-fetch.sh -s flickr -d "$HOME/Pictures/ServerFiles/Originals/%Y/%Y-%m-01-KelliPicasa" -u "41352893@N00" -L KelliFlickr
 jobRunner2.sh -n "flickr-dan" -t 1447  $flags $HOME/scripts/photo-fetch.sh -s flickr -d "$HOME/Pictures/ServerFiles/Originals/%Y/%Y-%m-01-DanFlickr" -u "49392099@N02" -L DanFlickr
 jobRunner2.sh -n "small-photos" -t 61 $flags $HOME/scripts/photo-thumbnails.sh "$HOME/Pictures/Photos/Originals" Originals SmallPhotos
 jobRunner2.sh -n "sync-appletv" -t 15  $flags $HOME/scripts/sync-appletv.sh
